ef8fdcb14d94d73af1b797de24a986a365590b19
mihirpatel9 <iammihirpatel9@gmail.com>
2024-03-04 11:13:36 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !password ||
+    !email ||
+    username === "" ||
+    password === "" ||
+    email === ""
+  ) {
+    return res.status(400).json({ message: "All Fields Are Required" });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json("Sign Up Successful");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
