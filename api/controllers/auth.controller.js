62f92a91dc2fd19cb2c315907a4d9212bc07c7fa
mihirpatel9 <iammihirpatel9@gmail.com>
2024-03-06 16:51:55 +0530

    add functionality to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,15 +6,14 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
-    !password ||
     !email ||
+    !password ||
     username === "" ||
-    password === "" ||
-    email === ""
+    email === "" ||
+    password === ""
   ) {
     next(errorHandler(400, "All fields are required"));
   }
-
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
