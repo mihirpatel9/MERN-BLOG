fa23990005308a8e3c87be45a920cc16bbe189da
mihirpatel9 <iammihirpatel9@gmail.com>
2024-03-09 14:27:33 +0530

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import React from 'react'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import Dashboard from './pages/Dashboard'
-import Project from './pages/Project'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/About' element={<About />}></Route>
-        <Route path='/Contact' element={<Contact />}></Route>
-        <Route path='/SignUp' element={<SignUp />}></Route>
-        <Route path='/SignIn' element={<SignIn />}></Route>
-        <Route path='/Dashboard' element={<Dashboard />}></Route>
-        <Route path='/Project' element={<Project />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/SignUp" element={<SignUp />}></Route>
+        <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+        <Route path="/Project" element={<Project />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsInstagram, BsYoutube, BsGithub, BsLinkedin } from "react-icons/bs";
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Mihir's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              {/* Third Section */}
+
+              <Footer.Title title="ABOUT" />
+              <Footer.LinkGroup>
+                <Footer.Link
+                  href="https://github.com/mihirpatel9/MERN-BLOG"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  BLOG APP Code
+                </Footer.Link>
+                <Footer.Link
+                  href="https://leetcode.com/mihirpatel9/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LEETCODE Profile
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+
+            {/* Second Section */}
+
+            <div>
+              <Footer.Title title="FOLLOW US" />
+              <Footer.LinkGroup>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/iammihirpatel9/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linkedin
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.youtube.com/@mihirpatelvlogs"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Youtube
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+
+            {/* Third Section */}
+
+            <div>
+              <Footer.Title title="LEGAL" />
+              <Footer.LinkGroup>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Mihir's Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsYoutube} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsLinkedin} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
