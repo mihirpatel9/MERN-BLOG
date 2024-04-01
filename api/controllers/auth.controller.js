b72c7425d800a6b6f4d1357e69ecfccaae5f4cf4
mihirpatel9 <iammihirpatel9@gmail.com>
2024-04-01 13:18:01 +0530

    add admin functionality to user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,10 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Credentials not found!"));
     }
-    const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = Jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     const { password: pass, ...rest } = validUser._doc;
     res
       .status(200)
