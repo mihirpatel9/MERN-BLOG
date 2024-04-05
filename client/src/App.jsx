a4dafcc3a38b231ad99df325214cabc92518b96b
mihirpatel9 <iammihirpatel9@gmail.com>
2024-04-05 15:35:53 +0530

    complete upload post image functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import Project from "./pages/Project";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
-import OnlyAdminPrivateRoute from "./components/onlyAdminPrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 
 export default function App() {
