afc1e71859f67a4a5027d1db11c8413323564c97
mihirpatel9 <iammihirpatel9@gmail.com>
2024-04-06 12:35:04 +0530

    add update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,10 +4,12 @@ import {
   create,
   deletepost,
   getposts,
+  updatepost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 export default router;
